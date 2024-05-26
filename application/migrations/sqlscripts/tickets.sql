cfee3849a80c5eced9bfc23d20661b5496e9eb15
Farhan Maba <muhammadfarhanulhassan@gmail.com>
2024-05-26 19:49:45 +0200

    feat: Add modified_by_id and timestamps to ospos_devices and ospos_tickets tables

--- application/migrations/sqlscripts/tickets.sql
+++ application/migrations/sqlscripts/tickets.sql
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS `ospos_devices` (
   `type` VARCHAR(255) NOT NULL,
   `serial` VARCHAR(255) DEFAULT NULL,
   `password` VARCHAR(255) DEFAULT NULL,
-  `password` VARCHAR(255) DEFAULT NULL
+  `modified_by_id` INT(10) NOT NULL,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Indexes for table `ospos_devices`
@@ -26,7 +28,7 @@ ALTER TABLE `ospos_devices`
 CREATE TABLE IF NOT EXISTS `ospos_tickets` (
   `id` INT(10) NOT NULL,
   `status` TINYINT(2) NOT NULL DEFAULT 0,
-  `problem` varchar(1024) DEFAULT NULL,
+  `problem` VARCHAR(1024) DEFAULT NULL,
   `initial_diagnosis` VARCHAR(1024) DEFAULT NULL,
   `completion_date` TIMESTAMP NULL,
   `due_date` TIMESTAMP NULL,
@@ -35,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `ospos_tickets` (
   `customer_id` INT(10) DEFAULT NULL,
   `assignee_id` INT(10) DEFAULT NULL,
   `employee_id` INT(10) NOT NULL,
-  `tax_code` varchar(32) NOT NULL,
+  `tax_code_id` INT(11) NOT NULL,
   `modified_by_id` INT(10) NOT NULL,
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -49,7 +51,7 @@ ALTER TABLE `ospos_tickets`
   ADD KEY `customer_id` (`customer_id`),
   ADD KEY `assignee_id` (`assignee_id`),
   ADD KEY `employee_id` (`employee_id`),
-  ADD KEY `tax_code` (`tax_code`),
+  ADD KEY `tax_code_id` (`tax_code_id`),
   ADD KEY `modified_by_id` (`modified_by_id`);
 -- AUTO_INCREMENT for table `ospos_tickets`
 ALTER TABLE `ospos_tickets`
@@ -60,7 +62,7 @@ ALTER TABLE `ospos_tickets`
   ADD CONSTRAINT `ospos_tickets_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `ospos_customers` (`person_id`),
   ADD CONSTRAINT `ospos_tickets_ibfk_3` FOREIGN KEY (`assignee_id`) REFERENCES `ospos_employees` (`person_id`),
   ADD CONSTRAINT `ospos_tickets_ibfk_4` FOREIGN KEY (`employee_id`) REFERENCES `ospos_employees` (`person_id`),
-  ADD CONSTRAINT `ospos_tickets_ibfk_5` FOREIGN KEY (`tax_code`) REFERENCES `ospos_tax_codes` (`tax_code`),
+  ADD CONSTRAINT `ospos_tickets_ibfk_5` FOREIGN KEY (`tax_code_id`) REFERENCES `ospos_tax_codes` (`tax_code_id`),
   ADD CONSTRAINT `ospos_tickets_ibfk_6` FOREIGN KEY (`modified_by_id`) REFERENCES `ospos_employees` (`person_id`);
 -- Table structure for table `ospos_tickets_parts`
 CREATE TABLE IF NOT EXISTS `ospos_tickets_parts` (
